a4fc05c2e68713de1c2dc85009bc6552e0da51b4
LuisChanocua <esteban083057@gmail.com>
2024-09-14 14:35:15 -0600

    feat(DTO'S): para la transferencia de infor controller vista

---
+++ CursoAngularNETCore_TiendaEnLinea/Tiendaenlinea.DTO/LoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiendaenlinea.DTO
+{
+    public class LoginDTO
+    {
+        public string Correo { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
---
+++ CursoAngularNETCore_TiendaEnLinea/Tiendaenlinea.DTO/ReporteDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiendaenlinea.DTO
+{
+    public class ReporteDTO
+    {
+        public string? NumeroDocumento {  get; set; }
+
+        public string? TipoPago { get; set; }
+
+        public string? FechaPago {  get; set; }  
+
+        public string? TotalVenta {  get; set; }
+
+        public string? Producto {  get; set; }
+
+        public int? Cantidad { get; set; }
+
+        public string? Precio {  get; set; }
+
+        public string? Total { get; set; }
+    }
+}
